37d214aad167f8a543bcd96b4b11254bc8feb44a
Marko Tričković <marko.trickovic13@gmail.com>
2021-02-13 15:26:36 +0100

    Add functionalities for update and remove the 'Teacher'

--- src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/alasdoo/developercourseassignment/service/impl/TeacherServiceImpl.java
@@ -41,10 +41,22 @@ public class TeacherServiceImpl implements TeacherService {
     }
 
     public void remove(Integer id) throws IllegalArgumentException {
+        Optional<Teacher> teacher = teacherRepository.findById(id);
+        if (!teacher.isPresent()) {
+            throw new IllegalArgumentException("Teacher with the following id = " + id + " is not found.");
+        }
+        teacherRepository.deleteById(id);
     }
 
     public TeacherDTO update(Integer id, TeacherDTO teacherDTO) {
-        return null;
+        Optional<Teacher> oldTeacher = teacherRepository.findById(id);
+        if (!oldTeacher.isPresent()) {
+            throw new IllegalArgumentException("Teacher with the following id " + id + " is not found.");
+        }
+        oldTeacher.get().setTeacherName(teacherDTO.getTeacherName());
+        oldTeacher.get().setTeacherSurname(teacherDTO.getTeacherSurname());
+        oldTeacher.get().setTeacherEmail(teacherDTO.getTeacherEmail());
+        return teacherMapper.transformToDTO(teacherRepository.save(oldTeacher.get()));
     }
 
     public TeacherDTO findByTeacherNameAndTeacherSurname(String name, String surname) {
@@ -58,7 +70,7 @@ public class TeacherServiceImpl implements TeacherService {
     public TeacherDTO findByTeacherEmail(String email) {
         Optional<Teacher> teacher = teacherRepository.findByTeacherEmail(email);
         if (!teacher.isPresent()) {
-            throw new IllegalArgumentException( ("Teacher with the following email = " + email + " is not found."));
+            throw new IllegalArgumentException("Teacher with the following email = " + email + " is not found.");
         }
         return teacherMapper.transformToDTO(teacher.get());
     }

d74bdf9cf2fc2d8e1c1d0236b84d8559770163fd
Marko Tričković <marko.trickovic13@gmail.com>
2021-02-15 19:03:49 +0100

    Make possible to run test in desired browser

--- src/test/java/environment/FunctionalTest.java
+++ src/test/java/environment/FunctionalTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -22,8 +25,29 @@ public class FunctionalTest {
 	@BeforeAll
 	public static void setUp() {
 		String path = System.getProperty("user.dir");
-		System.setProperty("webdriver.chrome.driver", path + "\\drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
+		String browser = System.getProperty("browser");
+
+		// Set up changeable browser by passed parameter
+		switch (browser.toLowerCase()) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", path + "\\drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			System.setProperty("webdriver.edge.driver", path + "\\drivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
+			break;
+		case "ie":
+			System.setProperty("webdriver.ie.driver", path + "\\drivers\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+			break;
+		case "firefox":
+		default:
+			System.setProperty("webdriver.gecko.driver", path + "\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		}
+		
 		driver.manage().window().setSize(new Dimension(1280, 768));
 		wait = new WebDriverWait(driver, 10);
 	}

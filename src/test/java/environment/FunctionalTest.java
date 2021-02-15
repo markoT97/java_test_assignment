aa433d37113dee095e2b338290b722c6580bdb87
Marko Tričković <marko.trickovic13@gmail.com>
2021-02-15 21:27:44 +0100

    Handle running test app without browser parameter

--- src/test/java/environment/FunctionalTest.java
+++ src/test/java/environment/FunctionalTest.java
@@ -21,31 +21,44 @@ public class FunctionalTest {
 
 	protected static WebDriver driver;
 	protected static WebDriverWait wait;
+	
+	private static final String PROJECT_PATH = System.getProperty("user.dir");
+	
+	
+	// Set default browser if parameter browser is not passed or if passed "firefox"
+	public static void setDefaultBrowser() {
+		System.setProperty("webdriver.gecko.driver", PROJECT_PATH + "\\drivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
 
 	@BeforeAll
 	public static void setUp() {
-		String path = System.getProperty("user.dir");
-		String browser = System.getProperty("browser");
-
-		// Set up changeable browser by passed parameter
-		switch (browser.toLowerCase()) {
-		case "chrome":
-			System.setProperty("webdriver.chrome.driver", path + "\\drivers\\chromedriver.exe");
-			driver = new ChromeDriver();
-			break;
-		case "edge":
-			System.setProperty("webdriver.edge.driver", path + "\\drivers\\msedgedriver.exe");
-			driver = new EdgeDriver();
-			break;
-		case "ie":
-			System.setProperty("webdriver.ie.driver", path + "\\drivers\\IEDriverServer.exe");
-			driver = new InternetExplorerDriver();
-			break;
-		case "firefox":
-		default:
-			System.setProperty("webdriver.gecko.driver", path + "\\drivers\\geckodriver.exe");
-			driver = new FirefoxDriver();
-			break;
+		
+		
+		try {
+			String browser = System.getProperty("browser");	
+			
+			// Set up changeable browser by passed parameter
+			switch (browser.toLowerCase()) {
+			case "chrome":
+				System.setProperty("webdriver.chrome.driver", PROJECT_PATH + "\\drivers\\chromedriver.exe");
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				System.setProperty("webdriver.edge.driver", PROJECT_PATH + "\\drivers\\msedgedriver.exe");
+				driver = new EdgeDriver();
+				break;
+			case "ie":
+				System.setProperty("webdriver.ie.driver", PROJECT_PATH + "\\drivers\\IEDriverServer.exe");
+				driver = new InternetExplorerDriver();
+				break;
+			case "firefox":
+			default:
+				setDefaultBrowser();
+				break;
+			}
+		} catch(NullPointerException ex) {
+			setDefaultBrowser();
 		}
 		
 		driver.manage().window().setSize(new Dimension(1280, 768));
